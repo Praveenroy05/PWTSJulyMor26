c3fe83e426c58db7e88b46e63630c653482ff49e
Praveen Roy <roykumar0593@gmail.com>
2026-07-24 08:53:16 +0530

    added statement

---
+++ Typescript/Test.js
@@ -0,0 +1,5 @@
+ let c = 10
+ c = null
+ console.log(d)
+
+console.log("Welcome to Typescript", 40, true)
---
+++ Typescript/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";
+// Datatype - What type of data a variable is storing
+// Java
+// int i = 10
+// Syntax:
+// Keyword(var/let/const) variableName:datatype(Optional) = value
+// TS -> Execute ->Converted into JS
+// let e:any = 10
+//  e = "TS"
+//  e = true
+// There are 2 types of datatypes:
+// 1. Primitive data type - Only a single value
+// 1. number
+// 2. string
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. union (|) - Combination of multiple datatype (number | string)
+// 7. any
+// 8. void
+// 1. number - Combination of integer(90,-87) and floating point number (67.345, -90.34)
+let num1 = 90.45;
+let num2 = 45;
+// typeof - which return the datatype of a variable
+console.log(typeof num1);
+console.log(typeof num2);
+// 2. Non-Primitive data type - More than 1 value
+// 1. Array
+// 2. String
+// 3. Object - {key:value}
