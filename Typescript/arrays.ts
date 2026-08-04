d41bb34bf958d5a6aebc5c5a4b2f465314049a4f
Praveen Roy <roykumar0593@gmail.com>
2026-08-04 08:37:25 +0530

    added array methods

--- Typescript/arrays.ts
+++ Typescript/arrays.ts
@@ -126,11 +126,92 @@ console.log(removedElement)
 console.log(arr5)
 
 
-// 3. unshift(ele1, ele2, ele3)
-// 4. shift()
+// 3. unshift(ele1, ele2, ele3,...) - It adds the element at the beginning of an array
+// Syntax:
+// arrayName.unshift(ele, ele1,...)
+arr5.unshift("JS", "Java")
+console.log(arr5);
+
+
+
+// 4. shift() - It removes the first element from an array
+// arrayName.shift()
+arr5.shift()
+console.log(arr5);
+
+
 
 // What is difference between push() and unshift() method
 // What is difference between pop() and shift()
+
+
+// 5. splice(startIndex, deleteCount, ele1, ele2, ele3,...) - Add/Remove the element
+// from the array at specified position
+
+// startIndex - The position where you want add/remove the element
+// deleteCount - The number of element to be deleted from an array
+// ele1, ele2,...(optional) - The element that you want to add at the startIndex
+
+arr5.splice(1, 1, 100,200,300)
+console.log(arr5);
+
+
+// 6. slice(startIndex?, endIndex?) - Returns the portion of element from 
+// startIndex to endIndex(Exclusive)
+
+let arr6 = [10,20,30,40,50,60,10,70,10]
+console.log(arr6)
+console.log(arr6.slice(2,5));
+
+// 7. indexOf(element, startIndex?) - Return the index of first occurances of an
+// element in an array, -1 if not found
+
+let arr7 = [10,20,30,40,50,60,10,70,10]
+
+console.log(arr7.indexOf(10))
+console.log(arr7.indexOf(10, 9))
+
+let index = arr7.indexOf(10)
+
+while(index != -1){
+    console.log(index)
+    index = arr7.indexOf(10, index+1)
+}
+
+
+// 8. lastIndexOf(element, startIndex?) - Return the index of last ocurances of 
+// an element inside an array
+console.log(arr6.lastIndexOf(10));
+
+// 9. includes(element, startIndex?) - Return true if the element is available
+// inside the array otherwise false
+console.log(arr6.includes(50))
+console.log(arr6.includes(500))
+
+// let arr7 = [10,20,30,40,50,60,10,70,10]
+
+
+// 10. join(separator?) - Joins all the elements of an array and return as a string
+let arr8 = [20,11,2026] // dd-mm-yyyy dd/mm/yyyy
+console.log(arr8.join("-"));
+
+// 11. toString() - Convert the array into a string
+console.log(arr8);
+console.log(arr8.toString());
+console.log(arr8);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
