32fe328bfa67517fd622769e12f2fdaf07fccd77
Praveen Roy <roykumar0593@gmail.com>
2026-09-25 09:48:34 +0530

    updated timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,7 +63,7 @@ export default defineConfig({
   reporter: [['html'], ['line'], ['allure-playwright']],
   timeout: 60000,
   expect:{
-    timeout: 5000
+    timeout: 15000
   },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
